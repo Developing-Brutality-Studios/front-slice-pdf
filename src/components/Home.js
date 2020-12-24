92a609d34df7b58f6afe30bf8a9ff65bb29b8912
pelanguero <pelanguero@gmail.com>
2020-12-24 18:41:27 -0500

    ajuste tarjetas y lector pdf

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,57 @@
-import { useState } from 'react';
-import axios from 'axios';
-import Carts from './Carts';
-import Sub from './Sub'
-import '../css/styles-carts.css'
-import { Link, Redirect } from 'react-router-dom'
+import { useState } from "react";
+import axios from "axios";
+import Carts from "./Carts";
+import Sub from "./Sub";
+import "../css/styles-carts.css";
+import { Link, Redirect } from "react-router-dom";
 const Home = () => {
-const [libros , setLibros] = useState([]);
-var arra=[];
-    if(libros.length == 0){
-            axios.get('http://localhost:8080/inicio', 
-            {
-                headers: {
-                    'token':  localStorage.getItem('Session')
-                }
-            }
-        
-        ).then((e) => {                     
-            setLibros(e.data.libros)
-            console.log("libros")
-            console.log(e.data)
-            arra=e.data.libros
-            console.log("arrrra")
-            console.log(arra)
-        }).catch(function (error) {
-            console.log(error);
-        })   
-    }
-    if(localStorage.getItem('Session')!=null){
-        console.log("arrrra")
-        console.log(arra)
-        return(<div>
+  const [libros, setLibros] = useState([]);
+  var arra = [];
+  if (libros.length == 0) {
+    axios
+      .get("http://localhost:8080/inicio", {
+        headers: {
+          token: localStorage.getItem("Session"),
+        },
+      })
+      .then((e) => {
+        setLibros(e.data.libros);
+        console.log("libros");
+        console.log(e.data);
+        arra = e.data.libros;
+        console.log("arrrra");
+        console.log(arra);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  if (localStorage.getItem("Session") != null) {
+    console.log("arrrra");
+    console.log(arra);
+    return (
+      <div>
         <div className="container-card">
-            {
-                libros.map((libro)=><Carts title={libro.Archivo} parrafo='Lorem Ipsum es simplemente el texto de relleno de las imp'  image={libro.Imagen}></Carts>)
-            }
-            
-            
+          <Sub
+            title="Agregar Libro"
+            image="http://localhost:8080/images/Plus_symbol.png"
+          ></Sub>
+          {libros.map((libro) => (
+            <Carts
+              key={libro.ID}
+              title={libro.Archivo}
+              parrafo="Lorem Ipsum es simplemente el texto de relleno de las imp"
+              image={libro.Imagen}
+            ></Carts>
+          ))}
         </div>
-        <br /><br />
-        
-    </div>);
-    } else{
-        return(
-            <Redirect to='/' />
-        )
-    }
-    
+        <br />
+        <br />
+      </div>
+    );
+  } else {
+    return <Redirect to="/" />;
+  }
+};
 
-
-}
-
-export default Home
+export default Home;
--- src/components/Pdf.js
+++ src/components/Pdf.js
@@ -1,125 +1,112 @@
-
-import axios from 'axios';
-import { useState } from 'react';
-import { Page } from 'react-pdf'
-import { Document } from 'react-pdf/dist/esm/entry.webpack';
-
-import '../css/styles-pdf.css';
-
+import axios from "axios";
+import { useState } from "react";
+import { Page } from "react-pdf";
+import { Document } from "react-pdf/dist/esm/entry.webpack";
+import React, { Component } from "react";
+import "../css/styles-pdf.css";
+import { useParams } from "react-router-dom";
 
 //import pdf from "http://example.com/sample.pdf"
 
-const MyApp = () => {
-    var lib  = ''   
+const MyApp = (props) => {
+  var lib = "";
+  var base = "http://localhost:8080/file/";
+  var params = useParams();
+  if (lib === "") {
+    axios
+      .get("http://localhost:4008/download", {
+        headers: {
+          token: localStorage.getItem("Session"),
+        },
+      })
+      .then((e) => {
+        lib = e.data;
+      });
+  }
 
-    if(lib === ''){
-        axios.get('http://localhost:4008/download', 
-        {
-            headers: {
-                'token': localStorage.getItem('Session')
-            }
-        }    
-    ).then((e) => {                       
-        lib = e.data
-        
-    })
-    }
-         
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [text, setText] = useState("");
+  const selectT = document.querySelectorAll(".selectect-text");
 
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-    const [text, setText] = useState('')
-    const selectT = document.querySelectorAll('.selectect-text')
-        
-    selectT.forEach(elem =>{
-        elem.addEventListener('mouseup', sText);
-    })
-    
-    function sText (event) {
-        const selectT = window.getSelection().toString().trim();
-        setText(selectT);
-    }
+  selectT.forEach((elem) => {
+    elem.addEventListener("mouseup", sText);
+  });
 
-    function onItemClick({ pageNumber: itemPageNumber }) {
-        setPageNumber(itemPageNumber);
-    }
+  function sText(event) {
+    const selectT = window.getSelection().toString().trim();
+    setText(selectT);
+  }
 
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-        setPageNumber(1);
-    }
+  function onItemClick({ pageNumber: itemPageNumber }) {
+    setPageNumber(itemPageNumber);
+  }
 
-    function changePage(offset) {
-        setPageNumber(prevPageNumber => prevPageNumber + offset);
-    }
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
 
-    function previousPage() {
-        changePage(-1);
-    }
+  function changePage(offset) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
 
-    function nextPage() {
-        changePage(1);
-    }  
-            
-    return (
-        <>
-            <div className='grid'>
-                <div>
-                    <p>
-                        Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-                    </p>
-                    <button
-                        type="button"
-                        disabled={pageNumber <= 1}
-                        onClick={previousPage}
-                    >
-                        Previous
-                    </button>
-                    <button
-                        type="button"
-                        disabled={pageNumber >= numPages}
-                        onClick={nextPage}
-                    >
-                        Next
-                    </button> 
-                    <div>
-                        <div>{text}</div>
-                    </div>
-                               
+  function previousPage() {
+    changePage(-1);
+  }
 
-                </div>
-                <div  className='selectect-text' >
-                    <Document
-                        file={'http://localhost:8080/file/DockerDeepDivebyNigelPoulton(z-lib.org).pdf'}
-                        onLoadSuccess={onDocumentLoadSuccess}
-                        onItemClick={onItemClick}
-                        className="document selectect-text"
-                        
-                    >
-                        <Page
-                            pageNumber={pageNumber}
-                            className='hoja  selectect-text' 
-                                                        
-                        />
-                    </Document>
+  function nextPage() {
+    changePage(1);
+  }
 
-                </div>
-            </div>
-
-        </>
-    );
-}
-
-const Pdf = () => {
-
-    return (
+  return (
+    <>
+      <div className="grid">
         <div>
-            <div className='hojas'>
-                <MyApp />
-            </div>
+          <p>
+            Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+          </p>
+          <button
+            type="button"
+            disabled={pageNumber <= 1}
+            onClick={previousPage}
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            disabled={pageNumber >= numPages}
+            onClick={nextPage}
+          >
+            Next
+          </button>
+          <div>
+            <div>{text}</div>
+          </div>
         </div>
-    )
+        <div className="selectect-text">
+          <Document
+            file={base.concat(params.nombrepdf)}
+            onLoadSuccess={onDocumentLoadSuccess}
+            onItemClick={onItemClick}
+            className="document selectect-text"
+          >
+            <Page pageNumber={pageNumber} className="hoja  selectect-text" />
+          </Document>
+        </div>
+      </div>
+    </>
+  );
+};
 
+export default class Pdf extends Component {
+  render() {
+    return (
+      <div>
+        <div className="hojas">
+          <MyApp />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default Pdf
